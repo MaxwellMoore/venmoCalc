532894118943cd95e9ad0a45a13cc52f25f2508e
Maxwell <maxwellmoore.work@gmail.com>
2025-03-17 23:17:59 -0600

    Initial Commit

---
+++ venmoCalc/venmoCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  venmoCalcApp.swift
+//  venmoCalc
+//
+//  Created by Maxwell Lane Moore on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct venmoCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ venmoCalcTests/venmoCalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  venmoCalcTests.swift
+//  venmoCalcTests
+//
+//  Created by Maxwell Lane Moore on 3/17/25.
+//
+
+import Testing
+@testable import venmoCalc
+
+struct venmoCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
